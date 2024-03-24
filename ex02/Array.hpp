1b83b721883f4574031636f5dd041761a4c8a667
Ramzi Chahbani <rchahban@c2r8p6.1337.ma>
2024-03-24 02:00:45 +0000

    added ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rchahban <rchahban@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/23 22:40:56 by rchahban          #+#    #+#             */
-/*   Updated: 2024/03/24 01:56:18 by rchahban         ###   ########.fr       */
+/*   Created: 2024/03/24 01:58:07 by rchahban          #+#    #+#             */
+/*   Updated: 2024/03/24 01:58:08 by rchahban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
-
-void addOne(int x) {
-	std::cout << x + 1 << std::endl;
-}
-
-int main( void ) {
-	int numbers[] = {1, 2, 3, 4};
-  	int size = sizeof(numbers) / sizeof(numbers[0]);
-
-  	::iter(numbers, size, addOne);
-}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,20 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rchahban <rchahban@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/23 22:40:56 by rchahban          #+#    #+#             */
-/*   Updated: 2024/03/24 01:56:18 by rchahban         ###   ########.fr       */
+/*   Created: 2024/03/24 01:58:09 by rchahban          #+#    #+#             */
+/*   Updated: 2024/03/24 01:58:10 by rchahban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
-
-void addOne(int x) {
-	std::cout << x + 1 << std::endl;
-}
-
-int main( void ) {
-	int numbers[] = {1, 2, 3, 4};
-  	int size = sizeof(numbers) / sizeof(numbers[0]);
-
-  	::iter(numbers, size, addOne);
-}
