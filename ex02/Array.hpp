9179ae99877ef99af1793e99c8c42c947684f78a
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-04-06 16:16:56 +0000

    progress in ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -15,21 +15,66 @@
 
 #include <iostream>
 
-template <typename T> class Array
+template <typename T> 
+class Array
 {
 	private:
-		unsigned int size;
+		T *arr;
+		unsigned int arrSize;
 	public:
-		Array();
-		~Array();
-		Array(unsigned int n);
-		Array(const Array& anotherArr);
-		Array& operator=(const Array& anotherArr);
-		unsigned int size();
+		Array()
+		{
+			this->arr = new T[this->arrSize];
+			std::cout << "Array constructor called" << std::endl;
+		}
+		~Array()
+		{
+			if (this->arrSize)
+				delete this->arrSize;
+			std::cout << "Array destructor called" << std::endl;
+		}
+		Array(unsigned int n) : arrSize(n)
+		{
+			this->arrSize = new T[this->arrSize];
+			std::cout << "Initialized array with " << this->arrSize << " elements." << std::endl;
+		}
+		Array(const Array& anotherArr)
+		{
+			*this = anotherArr;
+			std::cout << "Array copy constructor called" << std::endl;
+		}
+		Array& operator=(const Array& anotherArr)
+		{
+			if (this != &anotherArr)
+			{
+				this->arrSize = anotherArr.size();
+				this->arr = new T[this->size()];
+				for (int x = 0 ; x < this->arrSize; x++)
+				{
+					this->arr[x] = anotherArr.arr[x];
+				}
+			}
+		}
+		T& operator[](unsigned int index)
+		{
+			if (this->arr == NULL || index >= this->size())
+				throw IndexOutOfBoundsException();
+			return this->arr[index];
+		}
+		unsigned int size()
+		{
+			return this->arrSize;
+		}
 		class IndexOutOfBoundsException : public std::exception
 		{
 			virtual const char* what() const throw();
 		};
 };
+
+template< typename T >
+const char	*Array<T>::InvalidIndexException::what() const throw()
+{
+	return ("Error: Index out of bounds");
+}
 
 #endif
