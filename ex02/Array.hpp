7e23e21a9c2586de6cc9332a1929b630db9ad135
Ramzi Chahbani <rchahban@c2r1p8.1337.ma>
2024-04-06 02:54:12 +0000

    working on ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: rchahban <rchahban@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 01:58:07 by rchahban          #+#    #+#             */
-/*   Updated: 2024/03/28 21:39:33 by rchahban         ###   ########.fr       */
+/*   Updated: 2024/04/06 02:49:39 by rchahban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,19 @@
 
 template <typename T> class Array
 {
+	private:
+		unsigned int size;
 	public:
-		Array
+		Array();
+		~Array();
+		Array(unsigned int n);
+		Array(const Array& anotherArr);
+		Array& operator=(const Array& anotherArr);
+		unsigned int size();
+		class IndexOutOfBoundsException : public std::exception
+		{
+			virtual const char* what() const throw();
+		};
 };
 
 #endif
